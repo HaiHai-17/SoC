eab64438e2b22641512f83ee05b16fa1b16cf60f
HaiHai-17 <137904166+HaiHai-17@users.noreply.github.com>
2024-04-08 10:17:09 +0700

    8/4

---
+++ bai6/bai61/software/bai61/obj/default/bai61.d
@@ -0,0 +1,16 @@
+obj/default/bai61.o: bai61.c ../bai61_bsp//HAL/inc/sys/alt_stdio.h \
+  ../bai61_bsp/system.h ../bai61_bsp/linker.h \
+  ../bai61_bsp//drivers/inc/altera_avalon_uart_regs.h \
+  ../bai61_bsp//HAL/inc/io.h ../bai61_bsp//HAL/inc/alt_types.h
+
+../bai61_bsp//HAL/inc/sys/alt_stdio.h:
+
+../bai61_bsp/system.h:
+
+../bai61_bsp/linker.h:
+
+../bai61_bsp//drivers/inc/altera_avalon_uart_regs.h:
+
+../bai61_bsp//HAL/inc/io.h:
+
+../bai61_bsp//HAL/inc/alt_types.h:
